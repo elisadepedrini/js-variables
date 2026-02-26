f70bd61bfc1e8aeebe52fa2fb109abbad5f95f36
elisadepedrini <elisa.depedrini@gmail.com>
2026-02-26 11:40:43 +0100

    complete task 2 ex-02

--- ex-js-variables/02-strings/script.js
+++ ex-js-variables/02-strings/script.js
@@ -11,6 +11,13 @@ console.log(name);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+let number = 7
+let text = "hello"
+let bool = true
+console.log(number, text, bool);
+
+
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
